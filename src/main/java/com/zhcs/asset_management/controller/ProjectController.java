8cbd502514d2760c6c19c0f1cbeb3fc2d4ec44fd
grand <872849595@qq.com>
2018-11-08 18:20:12 +0800

    添加post api

--- src/main/java/com/zhcs/asset_management/controller/ProjectController.java
+++ src/main/java/com/zhcs/asset_management/controller/ProjectController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 @RequestMapping(value = "/project")
 @RestController
 public class ProjectController extends BaseController {
@@ -13,5 +18,18 @@ public class ProjectController extends BaseController {
     public String getProject(@PathVariable String id)
     {
         return id;
+    }
+
+    @RequestMapping(value="/add",method=RequestMethod.POST)
+    public void addProject(HttpServletRequest request, HttpServletResponse response)
+    {
+        try {
+            String content= request.getReader().readLine();
+            PrintWriter writer = response.getWriter();
+            writer.write(content);
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
